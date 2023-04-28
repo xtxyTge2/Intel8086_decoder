ba387e5798652307fa81107449cf96a5ebf5066f
xtxyTge2 <f1AZnn@protonmail.com>
2023-04-28 15:39:59 +0200

    Add Google Test Library to the project.

---
+++ src/instruction_info_test.cpp
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "instruction_decoder.hpp"
+
+#include "gtest/gtest.h"
+namespace {
+
+	TEST(simple_test, specific_test_name) 
+	{
+		EXPECT_EQ(1.0, 1.0);
+	}
+} // namespace
